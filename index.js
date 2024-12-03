7bd8005c8fba311736c4010cf15fe409c7dd107f
Surendra Dhaka <saabchoudhary350@gmail.com>
2024-12-03 12:07:46 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,29 @@
+const menuIcon=document.querySelector('#menu-icon');
+const navBar=document.querySelector('header nav');
+menuIcon.addEventListener('click',()=>{
+    menuIcon.classList.toggle('fa-x');
+    navBar.classList.toggle('active');
+})
+const activePage=()=>{
+    const header=document.querySelector('header');
+
+    menuIcon.classList.remove('fa-x');
+    navBar.classList.remove('active');
+}
+// resume
+const resumebtn=document.querySelectorAll(".resume-btn")
+resumebtn.forEach((btn,idx)=>{
+    btn.addEventListener('click',()=>{
+        const resumedetail = document.querySelectorAll('.resume-detail');
+        resumebtn.forEach(btn=>{
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+        resumedetail.forEach(detailss=>{
+            detailss.classList.remove("active");
+        });
+       resumedetail[idx].classList.add('active')
+       
+    })
+})
+
